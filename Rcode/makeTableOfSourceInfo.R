4f3a90e9e35cc967dfb9858dc34ff3006da70cfb
ylxdzsw <ylxdzsw@gmail.com>
2014-09-04 16:09:49 +0800

    make table of source info (mainly commits info)

--- Rcode/makeTableOfSourceInfo.R
+++ Rcode/makeTableOfSourceInfo.R
@@ -1,10 +1,23 @@
 makeTableOfSourceInfo <- function(){
+  Sys.setlocale("LC_TIME", "English")
   setwd("./Data/sourceCode")
   sourceName <- dir(".")
+  sourceInfo <- data.frame()
   for(i in 1:length(sourceName)){
     setwd(sourceName[i])
     commitInfo <- system("git log --pretty=format:\"%cd\"",intern=T)
-    #TODO#
+    commitInfo <- strsplit(commitInfo," ")
+    commitInfo <- sapply(commitInfo,function(x){paste(x[2],x[3],x[5])})
+    commitInfo <- as.Date(commitInfo,"%b %d %Y")
+    commitLm <- hist(commitInfo,10,plot=F)
+    commitLm <- lm(commitLm$counts ~ commitLm$mids)
+    sourceInfo <- rbind(sourceInfo,data.frame(coin=sourceName[i],
+                                              counts=length(commitInfo),
+                                              intercept=commitLm$coefficients[1],
+                                              coefficients.commitDate=commitLm$coefficients[2]))
+    setwd("../")
+    print(paste0("finished ",floor(100 * i / length(sourceName)),"%"))
   }
-  #TODO#
+  setwd("../../")
+  write.table(sourceInfo,"./Data/sourceInfo.txt")
 }
